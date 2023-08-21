d1f1ea8fe5d97bfccacafe663c637a929bae6c41
Davi Augusto <davi_cianorte@hotmail.com>
2023-08-21 00:57:41 -0300

    validacao dos campos do formulario2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
